653670209d2d3877d6ddcf6337d482c56c6c9699
Josep <>
2022-12-02 07:06:02 +0100

    create str_cleanup

---
+++ R/str_cleanup.R
@@ -0,0 +1,7 @@
+# Function to move from janitor::clean_names to names nice to plot.
+
+str_cleanup <- function (string)
+{
+  string <- stringr::str_replace_all("_", " ", string)
+  stringr::str_to_sentence(string)
+}
